c878cfe3e11e18e93676fb65f0fb3a00b2daf18a
VenkatMastercoder <venkatesangunaraj@gmail.com>
2023-08-15 12:38:48 +0530

    Updated Blog Page

--- src/Api/Card.js
+++ src/Api/Card.js
@@ -1,32 +1,45 @@
 export const Data = [
   {
-    id:"1",
-    title:"Basic of Git and Github",
-    content:"all the essential knowledge you need to become a Docker pro. From understanding containerization to mastering the key concepts",
-    imgLink:"https://res.cloudinary.com/dsq5spft3/image/upload/v1691855304/Rizzspace/1691251484239_ktrj82.jpg",
-    date:"10-10-2020",
-    blogLink:"https://venkatesan004.hashnode.dev/everything-about-git",
+    id: "1",
+    title: "Basic of Git and Github",
+    content: "all the essential knowledge you need to become a Docker pro. From understanding containerization to mastering the key concepts",
+    imgLink: "https://res.cloudinary.com/dsq5spft3/image/upload/v1691855304/Rizzspace/1691251484239_ktrj82.jpg",
+    date: "10-10-2020",
+    blogLink: "https://venkatesan004.hashnode.dev/everything-about-git",
   },
   {
-    id:"2",
-    title:"Basic of Git and Github",
-    content:"I dive into the world of Git, exploring its power and functionality for version control.",
-    imgLink:"https://res.cloudinary.com/dsq5spft3/image/upload/v1691855165/Rizzspace/1691147870743_t1tvfw.jpg",
-    date:"10-10-2020",
-    blogLink:"https://venkatesan004.hashnode.dev/everything-about-git",
+    id: "2",
+    title: "SpringBoot RESTful Architecture",
+    content: "the complexities of creating robust RESTful APIs using SpringBoot. From setting up the environment to implementing CRUD operations.",
+    imgLink: "https://res.cloudinary.com/dsq5spft3/image/upload/v1692083004/springBoot_gttdhh.jpg",
+    date: "10-10-2020",
+    blogLink: "https://tharunbalaji2004.hashnode.dev/springboot-restful-crud",
   },
   {
-    id:"3",
-    title:"Basic of Git and Github",
-    content:"I dive into the world of Git, exploring its power and functionality for version control.",
-    date:"10-10-2020",
-    blogLink:"https://venkatesan004.hashnode.dev/everything-about-git",
+    id: "3",
+    title: "Basic of Git and Github",
+    content: "I dive into the world of Git, exploring its power and functionality for version control.Git and understanding the three-stage architecture to creating snapshots and mastering branching and merging",
+    imgLink: "https://res.cloudinary.com/dsq5spft3/image/upload/v1691855165/Rizzspace/1691147870743_t1tvfw.jpg",
+    date: "10-10-2020",
+    blogLink: "https://venkatesan004.hashnode.dev/everything-about-git",
   },
-  {
-    id:"4",
-    title:"Basic of Git and Github",
-    content:"I dive into the world of Git, exploring its power and functionality for version control.",
-    date:"10-10-2020",
-    blogLink:"https://venkatesan004.hashnode.dev/everything-about-git",
-  }
 ];
+
+export const OfferData = [
+  {
+    imgSrc: "",
+    cardTitle: "Web Development",
+    cardContent: "CRM's and ERP Portfolio Development"
+  },
+  {
+    imgSrc: "",
+    cardTitle: "Mobile App Development",
+    cardContent: `IOS/Android - Flutter`
+  },
+  {
+    imgSrc: "",
+    cardTitle: "UI/UX Design",
+    cardContent: "User Stories UI/UX Design and User Testing"
+    //"User Stories and Scenarios Interaction Design Visual Design UI/UX Design User Testing"
+  }
+]
