eeb89a2db60afa010647f5baf2c757dacfed8cd8
VenkatMastercoder <venkatesangunaraj@gmail.com>
2023-08-15 13:59:33 +0530

    WhatWeOffer changed

--- src/Component/WhatWeOffer/WhatWeOffer.jsx
+++ src/Component/WhatWeOffer/WhatWeOffer.jsx
@@ -1,60 +1,35 @@
+import "./WhatWeOffer.css"
+import { OfferData } from "../../Api/Data";
 
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faMobileButton,faCode,faChartSimple,faPalette,faScrewdriverWrench} from '@fortawesome/free-solid-svg-icons'
-
-function WhatWeOffer() {
+const WhatWeOffer = () => {
   return (
-    <div className="App" id='services'>
-        <p className="Heading">What We Offer</p>
-        <section className='FlexContainer'>
-            <div className='flex-items one'>
-              <div className='vetor-image hidden md:flex'>
-                <FontAwesomeIcon icon={faMobileButton} className='icons'/>
-              </div>
-              <p className='title'>App Development</p>
-              <span></span>
-              <p className='content'>From concept to launch, our skilled app development team crafts seamless and innovative mobile and web applications ....</p>
-              <p className="read-more">Read More</p>
-            </div>
-            <div className='flex-items two'>
-                <div className='vetor-image hidden md:flex'>
-                  <FontAwesomeIcon icon={faCode} className='icons'/>
-                </div>
-                <p className='title'>Web Development</p>
-                <span></span>
-                <p className='content'>From concept to launch, our skilled app development team crafts seamless and innovative mobile and web applications ....</p>
-                <p className="read-more">Read More</p>
-              </div>
-            <div className='flex-items three'>
-                <div className='vetor-image hidden md:flex'>
-                <FontAwesomeIcon icon={faPalette} className='icons'/>
-                </div>
-                <p className='title'>UI/UX Designing</p>
-                <span></span>
-                <p className='content'>Our UI/UX designers turn user journeys into visually stunning and intuitive interfaces, elevating user engagement and .....</p>
-                <p className="read-more">Read More</p>
-              </div>
-            <div className='flex-items four '>
-              <div className='vetor-image hidden md:flex'>
-              <FontAwesomeIcon icon={faScrewdriverWrench} className='icons'/>
-              </div>
-              <p className='title'>Rebranding</p>
-              <span></span>
-              <p className='content'>Our UI/UX designers turn user journeys into visually stunning and intuitive interfaces, elevating user engagement and .....</p>
-              <p className="read-more">Read More</p>
-            </div>
-            <div className='flex-items five'>
-              <div className='vetor-image hidden md:flex'>
-              <FontAwesomeIcon icon={faChartSimple} className='icons'/>
-              </div>
-              <p className='title'>Market Research</p>
-              <span></span>
-              <p className='content'>Our UI/UX designers turn user journeys into visually stunning and intuitive interfaces, elevating user engagement and .....</p>
-              <p className="read-more">Read More</p>             
-            </div>
-        </section>
-    </div>
-  );
+    <>
+      <section className="py-10" id="services">
+        <div className="2xl:container mx-auto">
+          <div className="w-[90%] mx-auto grid grid-cols-1">
+            <p className="Heading">What We Offer</p>
+          </div>
+          <div className="w-[90%] mx-auto grid md:grid-cols-3 gap-5">
+            {
+              OfferData.map((e) => {
+                return (
+                  <>
+                    <div className="flex justify-center items-center">
+                      <div className="card min-h-[400px] flex flex-col justify-center items-center py-10 space-y-5 px-2">
+                        <div className="w-[114.19px] h-[100px] bg-zinc-700 rounded-full border border-black">{e.imgSrc}</div>
+                        <div className="text-white text-[25px] lg:text-[34px] font-['poppins'] leading-10 text-center break-words min-h-fit ">{e.cardTitle}</div>
+                        <div className="text-center text-zinc-400 text-[24px] font-normal break-words">{e.cardContent}</div>
+                      </div>
+                    </div>
+                  </>
+                )
+              })
+            }
+          </div>
+        </div>
+      </section>
+    </>
+  )
 }
 
-export default WhatWeOffer;
+export default WhatWeOffer
